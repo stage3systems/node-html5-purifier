5b11b4bba2d924983a01549962ee285234085f17
Ammar Vahanvaty <ammar@stage3systems.com>
2016-05-31 15:10:19 -0700

    Added failing test for ugc stripping

--- test/html_parser/index.js
+++ test/html_parser/index.js
@@ -54,4 +54,19 @@ describe('library - html purifier - html parser - parsePurified', function() {
     });
   });
 
+  it('should recursively remove multiple ugc tags without leaving dashes', function(done) {
+    var POSTFIX = 'ugc';
+    var PREFIX = 'ugc-';
+    var clean = '<p class="ugc ugc-ugc ugc-ugc-moz-cite-prefix ugc-ugc-ugc-ugc-ugc-MsoNormal">Text</p>';
+
+      var expected = '<p class="moz-cite-prefix MsoNormal>Text</p>';
+
+      parser.parsePurified(clean, PREFIX, POSTFIX, function(err, stripped) {
+        expect(stripped).to.equal(expected);
+        done();
+      });
+
+    });
+
+
 });
