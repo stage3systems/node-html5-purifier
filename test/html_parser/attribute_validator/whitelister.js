26af7a9743fd90cf6e2e4c83bce25e924d5f4e2e
tajinder <tajinder@stage3systems.com>
2018-08-23 12:07:10 +0530

    SED-4064 allow rowspan attribute in html

--- test/html_parser/attribute_validator/whitelister.js
+++ test/html_parser/attribute_validator/whitelister.js
@@ -68,6 +68,10 @@ describe('library - html purifier - attribute validator - whitelister', function
       var result = whitelister.verify('src');
       expect(result).to.be.ok();
     });
+    it('should allow `rowspan`', function() {
+      var result = whitelister.verify('rowspan');
+      expect(result).to.be.ok();
+    });
   });
 
 });
